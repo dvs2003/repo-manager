f5b83adb62993934c601f137d9f6e182432b2f1d
dvs2003 <50419946+dvs2003@users.noreply.github.com>
2022-05-09 00:47:44 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -14,3 +14,19 @@ delete repo rm -f /etc/yum.repos.d/<name-of-repository-file>
 
 now for ubuntu, arch, gentoo PaIn
 """
+
+import subprocess
+
+distro = []
+
+def list_all_repos():
+    subprocess.run(['sudo dnf repo list'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+
+def list_all_gpg():
+    print('OFFICIAL')
+    subprocess.run(["rpm -q gpg-pubkey --qf '%{NAME}-%{VERSION}-%{RELEASE}\t%{SUMMARY}\n'"], stdout=subprocess.PIPE).stdout.decode('utf-8')
+
+def delete_gpg(gpgkey):
+    subprocess.run([f'rpm -e {gpgkey}'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+#if rpm can be replaced with deb and stuff
+#CHECK THE DISTRO
